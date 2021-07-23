73ec6add4b846262b0f7a18b24ae1207ac562585
midori-design <m.midori1115@icloud.com>
2021-07-23 23:38:20 +0900

    add neon CSS and favicon

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,81 @@
 <template>
+  <div>
   <div id="app">
     <div class="main-area">
       <div class="header">
         <div>About</div>
         <div>Works</div>
-        <div>Service</div>      
+        <div>Service</div>
         <div>Contact</div>
       </div>
-      <div class=top-img>
-        <img src="~@/assets/background.jpg">
+      <div class="top-img">
+        <img class="img" src="~@/assets/background.jpg">
         <div class="gray-mask">
           <div>SHIBUYA</div>
           <div>TOKYO</div>
-          <button>Watch Goggle Map</button>
         </div>
       </div>
-      <div class="about">
-      About This Site
+      <div class="section">
+        <div class="section-title">
+        About This Site
+        </div>
+        This site was implemented to convey the image of SHIBUYA, TOKYO.<br>
+        Please let me know if there is anything I should do to make the design look cooler.<br>
+        There's no contact page yet, though.            
       </div>
+      <div class="section">
+        <div class="section-title">
+          Location
+        </div>
+        <google-map id="map" style="margin-top: 30px;"/>  
+      </div>
+      <div class="section">
+        <div class="section-title">
+        Feature
+        </div>
+        Shibuya station is too crowded, right?<br>
+
+        For more information about Shibuya-ku, click on the link below.        
+        <a class ="Link" href="https://www.city.shibuya.tokyo.jp/">
+        The official website of Shibuya-ku.
+        </a>
+      </div>
+      <div class="section" style="height: 100px;">
+        <div class="section-title">
+        Contact
+        </div>
+        I'll build this section, if I get around to it...  
+      </div>
+      <div class=footer>©︎it's me</div>
     </div>
+  </div>
+
   </div>
 </template>
 
 <script>
+import GoogleMap from './components/GoogleMap.vue'
 export default {
   name: "App",
-  components: {},
+  components: {
+    "google-map" : GoogleMap
+  },
   data() {
     return {
-      msg: "I am Midori.",
-      question: "Who are you?",
     };
   },
   methods: {
-    handleClick() {
-      alert("I'm midori!");
+    showMap(){
+        window.scrollTo({
+        top: 800,
+        behavior: "smooth"
+      });
+      
     },
+    mapClick (event) {
+      console.log(event);
+      //$event.latLngにクリック地点が入っている
+    }    
   },
 };
 </script>
@@ -49,7 +89,7 @@ export default {
   color: #2c3e50;
 }
 .main-area {
-  background-color: rgb(255, 255, 255);
+  background-color: rgb(0, 0, 0);
   width: 100%;
   min-height: 700px;
   background-size: cover;
@@ -60,7 +100,7 @@ export default {
   justify-content :flex-end;
   margin-right: 4px;
 }
-.header > div {
+.header > div, a {
   display: flex;
   width: 100px;
   height: 30px;
@@ -71,17 +111,42 @@ export default {
   justify-content: center;
   align-items: center;
   color: black;
+  cursor: pointer;
+
+  font-style: italic;
+  color: #fff;
+  border: 0.2rem solid #fff;
+  border-radius: .5rem;
+  text-transform: uppercase;
+  animation: header_flicker 1.5s infinite alternate;     
+
 }
+.header > div::-moz-selection {
+  background-color: var(--neon-border-color);
+  color: var(--neon-text-color);
+}
+
+.header > div::selection {
+  background-color: var(--neon-border-color);
+  color: var(--neon-text-color);
+}
+
 .header > div:last-child{
-  background-color: rgba(23, 76, 83, 0.697);
+  background-color: rgba(255, 255, 255, 0.697);
   color: white;
 }
+
+.header > div:hover {
+  background-color: rgba(255, 255, 255, 0.697);
+  color: white;
+}
+
 .top-img{
   position: relative;
   width: 100%;
   height: auto;
 }
-img {
+.img {
   display: block;
   width: 100%;
   min-width: 1000px;
@@ -100,27 +165,95 @@ img {
   min-width: 380px;
   height: 100%;
   background-color: gray;
-  opacity: .75;
+  opacity: .65;
   font-size: 60px;
-  font-weight: bold;
+  /* font-weight: 300px; */
   color: white;
 }
-.about {
-  display: block;
-  width: 350px;
+.section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 70px;
+  font-size: 20px;  
+  color: #fff;  
+}
+.section-title {
   font-size: 36px;
   font-weight: bold;
-  margin-top: 50px;
-  margin-left: 50px;
+  margin-bottom: 20px;
+  color: #fff;
+  animation: flicker 1.5s infinite alternate;
 }
-button {
-  margin-top: 40px;
-  width: 250px;
-  height:40px;
-  font-size: 20px;
-  color: black;
-  background-color: rgb(158, 218, 227);
-  border-radius: 30px;
-  font-weight: bold;
+.footer{
+  margin-top: 20px;
+  color: #fff;
+  font-size: 12px;  
+  height: 50px;
 }
+
+:root {
+  /* Base font size */
+  font-size: 10px;   
+  
+  /* Set neon color */
+  --neon-text-color: rgb(42, 90, 221);
+  --neon-border-color: rgb(12, 161, 129);
+}
+a{
+  width: 300px;
+  height: 30px;
+  margin-top: 30px;
+}
+
+/* Animate neon flicker */
+@keyframes header_flicker {
+    
+    0%, 19%, 21%, 23%, 25%, 54%, 56%, 100% {
+      
+        text-shadow:
+            -0.2rem -0.2rem 1rem #fff,
+            0.2rem 0.2rem 1rem #fff,
+            0 0 2rem var(--neon-text-color),
+            0 0 4rem var(--neon-text-color),
+            0 0 6rem var(--neon-text-color),
+            0 0 8rem var(--neon-text-color),
+            0 0 10rem var(--neon-text-color);
+        
+        box-shadow:
+            0 0 .5rem #fff,
+            inset 0 0 .5rem #fff,
+            0 0 2rem var(--neon-border-color),
+            inset 0 0 2rem var(--neon-border-color),
+            0 0 4rem var(--neon-border-color),
+            inset 0 0 4rem var(--neon-border-color);        
+    }
+    
+    20%, 24%, 55% {        
+        text-shadow: none;
+        box-shadow: none;
+    }    
+}
+@keyframes flicker {
+    
+  0%, 18%, 22%, 25%, 53%, 57%, 100% {
+
+      text-shadow:
+      0 0 4px #fff,
+      0 0 11px #fff,
+      0 0 19px #fff,
+      0 0 40px #0fa,
+      0 0 80px #0fa,
+      0 0 90px #0fa,
+      0 0 100px #0fa,
+      0 0 150px #0fa;
+  
+  }
+  
+  20%, 24%, 55% {        
+      text-shadow: none;
+  }    
+}
+
 </style>
